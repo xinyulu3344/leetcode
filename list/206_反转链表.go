caa1a79252cb0dbd579d1fc3e093a435b8d2f1c0
xinyulu <xinyulu3344@163.com>
2022-09-23 00:24:50 +0800

    206 迭代 递归

--- list/206_反转链表.go
+++ list/206_反转链表.go
@@ -1,6 +1,5 @@
 package list
 
-
 // https://leetcode.cn/problems/reverse-linked-list/
 
 /**
@@ -11,34 +10,46 @@ package list
  * }
  */
 func reverseList(head *ListNode) *ListNode {
-	m := make([]int, 0)
-	tmp1Head := head
-	tmp2Head := head
-	for {
-		if head == nil {
-			break
-		}
-		m = append(m, head.Val)
-		head = head.Next
-	}
-	for i := len(m)-1; i >= 0; i-- {
-		if tmp1Head == nil {
-			break
-		}
-		tmp1Head.Val = m[i]
-		tmp1Head = tmp1Head.Next
-	}
-	return tmp2Head
+    m := make([]int, 0)
+    tmp1Head := head
+    tmp2Head := head
+    for {
+        if head == nil {
+            break
+        }
+        m = append(m, head.Val)
+        head = head.Next
+    }
+    for i := len(m) - 1; i >= 0; i-- {
+        if tmp1Head == nil {
+            break
+        }
+        tmp1Head.Val = m[i]
+        tmp1Head = tmp1Head.Next
+    }
+    return tmp2Head
 }
 
+// 迭代解法
 func reverseList1(head *ListNode) *ListNode {
-	var prev *ListNode
-	curr := head
-	for curr != nil {
-		next := curr.Next
-		curr.Next = prev
-		prev = curr
-		curr = next
-	}
-	return prev
+    var prev *ListNode
+    for head != nil {
+        next := head.Next
+        head.Next = prev
+        prev = head
+        head = next
+    }
+    return prev
+}
+
+// 递归解法
+func reverseList2(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {
+        return head
+    }
+    newHead := reverseList2(head.Next)
+    head.Next.Next = head
+    head.Next = nil
+    
+    return newHead
 }
